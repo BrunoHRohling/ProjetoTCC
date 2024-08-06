f765604c75324c44e81965ec6eeb8f39fd50f2d9
Bazadu <aluno@example.com>
2024-08-05 21:30:18 -0300

    Fiz parcialmente o frontend do index/register

--- themes/web/index.php
+++ themes/web/index.php
@@ -9,19 +9,30 @@ if (isset($_POST['submit'])) {
     $user = new User($_POST['name'], $_POST['email'], $_POST['password'], $_POST['confirmPassword']);
     $user->register();
     echo $user->getMessage();
-
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Re-IFeitório</title>
     <link rel="stylesheet" href="../../shared/styles/styles.css">
 </head>
+
 <body>
-<h1>Bem vindo ao website</h1>
-<?php include "register.php"; ?>
+    <div id="welcomeH1">
+        <h1>Seja bem vindo!</h1>
+    </div>
+
+    <img src="../../storage/images/whiteLogo.png" style="position:absolute; top:-4%; left:53%" height="300" width="760">
+
+    <?php
+        include "register.php";  
+    ?>
+
 </body>
+
 </html>
--- themes/web/register.php
+++ themes/web/register.php
@@ -1,13 +1,24 @@
-<h2>Aqui é o cadastro</h2>
-<form action="" method="post" autocomplete="off">
-    <label for="name">Nome:</label>
-    <input type="text" name="name" id="name" required value=""> <br>
-    <label for="email">E-mail:</label>
-    <input type="email" name="email" id="email" required value=""> <br>
-    <label for="password">Senha:</label>
-    <input type="password" name="password" id="password" required value=""> <br>
-    <label for="confirmPassword">Confirme a senha:</label>
-    <input type="password" name="confirmPassword" id="confirmPassword" required value=""> <br>
-    <button type="submit" name="submit">Cadastrar-se</button>
-</form>
-<p>Já possui uma conta? Faça <a href="login.php">Login</a></p>
+<div class="form-register">
+    <h2 style="color:white">cadastragens e sapecagens</h2>
+    <h3 style="color:gray">(placeholder)</h3>
+
+    <form action="" method="post" autocomplete="off">
+        <!-- <label for="name">Nome:</label> -->
+        <input type="text" name="name" id="name" placeholder="Nome" required> <br>
+
+        <!--<label for="email">E-mail:</label>-->
+        <input type="email" name="email" id="email" placeholder="E-mail" required> <br>
+
+        <!--<label for="password">Senha:</label>-->
+        <input type="password" name="password" id="password" placeholder="Senha" required> <br>
+
+        <!--<label for="confirmPassword">Confirme a senha:</label>-->
+        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmar Senha" required> <br>
+
+
+        <button type="submit" name="submit">Criar Conta</button>
+    </form>
+
+    
+    <p style="color:white">Já possui uma conta? Faça <a style="color:cyan" href="login.php">Login</a></p>
+</div>
